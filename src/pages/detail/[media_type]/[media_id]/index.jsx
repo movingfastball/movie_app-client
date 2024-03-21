1fda7b6e4e9d59d2afa0f03ec2b3c9cc29314e82
movingfastball <movingfastballny2012@yahoo.co.jp>
2024-03-21 10:26:50 +0900

    10-82

--- src/pages/detail/[media_type]/[media_id]/index.jsx
+++ src/pages/detail/[media_type]/[media_id]/index.jsx
@@ -159,11 +159,21 @@ const Detail = ({detail, media_type, media_id}) => {
   useEffect(() => {
     const fetchReviews = async() => {
       try {
-        const response = await laravelAxios.get(`api/reviews/${media_type}/${media_id}`)
+        const [reviewResponse, favoriteResponse] = await Promise.all([
+          laravelAxios.get(`api/reviews/${media_type}/${media_id}`),
+          laravelAxios.get('api/favorites/status',{
+            params:{
+              media_type: media_type,
+              media_id: media_id
+            }
+          })
+        ]) 
         console.log(response.data);
-        const fetchReviews = response.data;
-        setReviews(response.data)
-        updateAverageRating(response.data)
+        const fetchReviews = reviewResponse.data;
+        setReviews(fetchReviews)
+        updateAverageRating(fetchReviews)
+
+        
       } catch(err) {
         console.log(err)
       }
