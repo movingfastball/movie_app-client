43fcbda5b2528ef4fa133c07c432b435ad6a9e2c
movingfastball <movingfastballny2012@yahoo.co.jp>
2024-03-22 18:04:44 +0900

    10-89

--- src/pages/favorites.jsx
+++ src/pages/favorites.jsx
@@ -1,7 +1,7 @@
 import AppLayout from '@/components/Layouts/AppLayout'
 import MediaCard from '@/components/MediaCard';
 import laravelAxios from '@/lib/laravelAxios';
-import { Container, Grid } from '@mui/material';
+import { Container, Grid, Typography } from '@mui/material';
 import Head from 'next/head'
 import React from 'react'
 import useSWR from 'swr';
@@ -13,6 +13,9 @@ const favorites = () => {
 
     console.log(favoriteItems);
     console.log(error);
+
+    const loading = !favoriteItems && !error;
+
     if(error) {
         return <div>エラーが発生しました</div>
     }
@@ -31,11 +34,22 @@ const favorites = () => {
         </Head>
     
         <Container>
-            <Grid container spacing={3} py={3}>
-                {favoriteItems?.map((item) => (
-                    <MediaCard item={item} key={item.id} isContent={false}/>
-                ))}
-            </Grid>
+            {loading ? (
+                <Grid item textAlign={"center"} xs={12}>
+                <Typography>Loading...</Typography>
+                </Grid>
+            ) : favoriteItems.length > 0 ?(
+                <Grid container spacing={3} py={3}>
+                    {favoriteItems?.map((item) => (
+                        <MediaCard item={item} key={item.id} isContent={false}/>
+                    ))}
+                </Grid>
+            ) : (
+                <Grid item textAlign={"center"} xs={12}>
+                    <Typography>お気に入り登録が見つかりませんでした</Typography>
+                </Grid>
+            )}
+
         </Container>
     </AppLayout>
   )
